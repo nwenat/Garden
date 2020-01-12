c09fae41afee276bf1d76c3cbfe36e99542a0ab7
Natalia Dorozynska <natala.dorozynska@gmail.com>
2020-01-12 19:55:04 +0100

    Basic menu

--- garden/src/pl/edu/pl/Menu.java
+++ garden/src/pl/edu/pl/Menu.java
@@ -4,41 +4,73 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Menu {
-    public enum Options {
-        CLOSE(0, "Zakoncz program"),
-        ADD_USER(1, "Dodaj wlasciciela"),
-        REMOVE_USER(2, "Usun wlasciciela"),
-        LIST_OF_USERS(3, "Wyswietl liste wlascicieli");
+    public enum Option {
+        UNKNOW("-1", "Blad"),
+        CLOSE("0", "Zakoncz program"),
+        ADD_USER("1", "Dodaj wlasciciela"),
+        REMOVE_USER("2", "Usun wlasciciela"),
+        LIST_OF_USERS("3", "Wyswietl liste wlascicieli");
 
-        private int optionId;
+        private String optionId;
         private String valuePL;
 
-        Options(int optionId, String valuePL) {
+        Option(String optionId, String valuePL) {
             this.optionId = optionId;
             this.valuePL = valuePL;
+        }
+
+        public static Option fromId(String id){
+            return Arrays.stream(Option.values())
+                    .filter(o -> o.optionId.equals(id))
+                    .findFirst()
+                    .orElse(UNKNOW);
         }
 
         public String getValuePL() {
             return valuePL;
         }
 
-        public int getOptionId() {
+        public String getOptionId() {
             return optionId;
         }
     }
 
-    private int selectedOptions;
+
 
     public void showMenu(Scanner scanner){
+
+        Option selectedOption;
+        do{
+            printMenu();
+            selectedOption = Option.fromId(scanner.nextLine());
+
+            switch (selectedOption) {
+                case CLOSE:
+                    System.out.println("Zapisanie i zamknięnie programu ///////");
+                    break;
+                case ADD_USER:
+                    System.out.println("Dodanie uzytkownika ///////");
+                    break;
+                case REMOVE_USER:
+                    System.out.println("Usunięcie uzytkownika ///////");
+                    break;
+                case LIST_OF_USERS:
+                    System.out.println("Wyświetlenie listy uzytkownikow ///////");
+                    break;
+                case UNKNOW:
+                    System.out.println("Blednie wybrana opcja");
+                    break;
+            }
+
+        }while (!selectedOption.equals(Option.CLOSE));
+
+    }
+
+    private void printMenu(){
         System.out.println("-------------MENU-------------");
-        Arrays.stream(Options.values())
+        Arrays.stream(Option.values())
+                .filter(o -> !o.getOptionId().equals("-1"))
                 .forEach(o -> System.out.println(o.getOptionId() + " - " + o.valuePL));
         System.out.println("Wybierz opcje: ");
-
-        selectedOptions = scanner.nextInt();
-
-
-
-
     }
 }
